258697bf060af6192a03e18d40119db251e81356
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-07-26 17:50:58 +0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from .resume_info import main as resume_info
+from .vacancy_info import main as vacancy_info
 from .main import main as main
 
 
@@ -8,5 +9,6 @@ Page = namedtuple("Page", "title method")
 pages: dict[str, Page] = {
     'Главная': Page(title="Главная", method=main),
     'Resume_info': Page(title="Resume info", method=resume_info),
+    'Vacancy_info': Page(title="Vacancy info", method=vacancy_info),
 
 }
