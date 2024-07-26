48f19e6a32bf50ae467bc63d95bbd19e8d883ffe
User Y1OV <sukolov-21@edu.ranepa.ru>
2024-07-26 15:21:26 +0300

    first commit

---
+++ zapusk.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import streamlit_antd_components as sac
+from pages import pages
+
+def main() -> None:
+    if 'index' not in st.session_state:
+        st.session_state['index'] = 0
+
+    with st.sidebar.container():
+        menu = sac.menu(
+            items=list(pages.keys()),  # Убедитесь, что ключи словаря pages правильные
+            index=st.session_state['index'],
+            open_all=True,
+            size='middle',
+            format_func=lambda page: pages[page].title,
+        )
+        st.sidebar.info("Made with ❤️ by the ikanam")
+
+    with st.container():
+        if menu in pages:
+            pages[menu].method()
+ы
+if __name__ == "__main__":
+    main()
