289575d99d85e43109b0e6372cf0effc9731b2ce
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-07-26 16:18:40 +0300

    Update zapusk.py

--- zapusk.py
+++ zapusk.py
@@ -3,16 +3,14 @@ import streamlit_antd_components as sac
 from pages_ import pages
 
 
-
 def main() -> None:
-    # if 'index' not in st.session_state:
-    #     st.session_state['index'] = 0
+    if 'index' not in st.session_state:
+        st.session_state['index'] = 0
 
     with st.sidebar.container():
         menu = sac.menu(
             items=list(pages.keys()),
-            # index=st.session_state['index'],
-            index=1,
+            index=st.session_state['index'],
             open_all=True,
             size='middle',
             format_func=lambda page: pages[page].title,
@@ -22,7 +20,3 @@ def main() -> None:
     with st.container():
         if menu in pages:
             pages[menu].method()
-
-
-if __name__ == "__main__":
-    main()
