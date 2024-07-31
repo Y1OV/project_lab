857b5ea2ac005769101fbbb26516dcc65929170c
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-07-31 15:40:33 +0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from .resume_info import main as resume_info
 from .vacancy_info import main as vacancy_info
+from .HHapi import main as HHapi
 from .main import main as main
 
 
@@ -10,5 +11,6 @@ pages: dict[str, Page] = {
     'Главная': Page(title="Главная", method=main),
     'Resume_info': Page(title="Resume info", method=resume_info),
     'Vacancy_info': Page(title="Vacancy info", method=vacancy_info),
+    'HHapi': Page(title="HeadHunter API", method=HHapi),
 
 }
