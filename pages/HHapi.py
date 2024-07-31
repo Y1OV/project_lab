ae7de4f138bbe169d0c50db90679292777f53bf3
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-07-31 15:39:33 +0300

    Create HHapi.py

---
+++ pages/HHapi.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+html_code = '''
+<div style="text-align: center;">
+    <a href="https://raw.githubusercontent.com/Y1OV/project_lab/main/data/ranepa.png">
+        <img src="https://raw.githubusercontent.com/Y1OV/project_lab/main/data/ranepa.png" alt="Foo" style="width: 50%; height: auto;">
+    </a>
+</div>
+'''
+
+def main():
+    
+    st.write("""HeadHunter API — это инструментарий для интеграции [HeadHunter](http://hh.ru/) в ваш продукт.""")
+
+
+if __name__ == "__main__":
+    main()
